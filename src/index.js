baa31e101347861fd9a4a08240cabb0b9c6630b3
Lucas Almeida <luccasja@live.com>
2020-04-26 15:50:00 -0300

    Implementação da pagina Home em bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './routes';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './global.css'
+import Routes from './routes';
+
 
 ReactDOM.render(<Routes/>,document.getElementById('root'));
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './style.css'
 import Logo from '../../img/Logo.jpeg'
 import {useHistory} from 'react-router-dom'
+import { Button, Container, Row, Col, Image} from 'react-bootstrap';
 
 const Home = () => {
     const history = useHistory()
@@ -11,19 +12,24 @@ const Home = () => {
     }
 
     return(
-        <div className='container-geral'>
-            <img src={Logo} alt='logo'/>
-            <div className='info'>
-                <div className='saudacao'>
-                    <p className='saudacao-txt'>Olá, seja bem-vindo(a) ao <strong>Fina Massa</strong></p>
-                    
-                    <p style={{fontSize:20}} ><strong>Horário de Funcionamento</strong></p>
-                    <p>Segunda a Quinta das 16:00 às 20:00</p>
-                    <p>Sexta, Domingo e Feriados das 16:00 às 22:00</p>
-                </div>
-            </div>
-            <button onClick={handleNovoPedido}>Novo Pedido</button>
-        </div>
+        <Container>
+            <Row style={{textAlign:'center', marginTop:70}}>
+                <Col/>
+                <Col md="auto" style={{background:'#FFF', padding:70, borderRadius:8}}>
+                <Image src={Logo} style={{height:220, marginBottom:40, width:220}} alt='logo' />
+                <Row style={{textAlign:'center', background:'#FCFCFC', padding:20, marginBottom:40, borderRadius:8, borderStyle:'solid', borderWidth:0.5, borderColor:'#e3e3e3'}}>
+                    <Col>
+                        <p className='saudacao-txt' style={{marginBottom:15, fontSize:20}}>Olá, seja bem-vindo(a) ao <strong>Fina Massa</strong></p>
+                        <p style={{fontSize:20}} ><strong>Horário de Funcionamento</strong></p>
+                        <p>Segunda a Quinta das 16:00 às 20:00</p>
+                        <p>Sexta, Domingo e Feriados das 16:00 às 22:00</p>
+                    </Col>
+                </Row>
+                <Button style={{background:'#FF414D', border:0, width:200, borderRadius:8 }} onClick={handleNovoPedido}>Novo Pedido</Button>
+                </Col>
+                <Col/>
+            </Row>
+        </Container>
     )
 };
 
