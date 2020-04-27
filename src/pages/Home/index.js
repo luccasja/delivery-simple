20f39ebaa848851ee631a8d0a60e155aafd03458
Lucas Almeida <luccasja@live.com>
2020-04-26 21:39:51 -0300

    implentada a janela Pedido com Bootstrap

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,19 +13,19 @@ const Home = () => {
 
     return(
         <Container>
-            <Row style={{textAlign:'center', marginTop:70}}>
+            <Row style={{textAlign:'center'}}>
                 <Col/>
-                <Col md="auto" style={{background:'#FFF', padding:70, borderRadius:8}}>
-                <Image src={Logo} style={{height:220, marginBottom:40, width:220}} alt='logo' />
-                <Row style={{textAlign:'center', background:'#FCFCFC', padding:20, marginBottom:40, borderRadius:8, borderStyle:'solid', borderWidth:0.5, borderColor:'#e3e3e3'}}>
-                    <Col>
-                        <p className='saudacao-txt' style={{marginBottom:15, fontSize:20}}>Olá, seja bem-vindo(a) ao <strong>Fina Massa</strong></p>
-                        <p style={{fontSize:20}} ><strong>Horário de Funcionamento</strong></p>
-                        <p>Segunda a Quinta das 16:00 às 20:00</p>
-                        <p>Sexta, Domingo e Feriados das 16:00 às 22:00</p>
-                    </Col>
-                </Row>
-                <Button style={{background:'#FF414D', border:0, width:200, borderRadius:8 }} onClick={handleNovoPedido}>Novo Pedido</Button>
+                <Col md="auto" style={{background:'#FFF', padding:40, borderRadius:8}}>
+                    <Image src={Logo} style={{height:150, marginBottom:20, width:150}} alt='logo' />
+                    <Row style={{textAlign:'center', background:'#FCFCFC', padding:20, marginBottom:40, borderRadius:8, borderStyle:'solid', borderWidth:0.5, borderColor:'#e3e3e3'}}>
+                        <Col>
+                            <p className='saudacao-txt' style={{marginBottom:15, fontSize:20}}>Olá, seja bem-vindo(a) ao <strong>Fina Massa</strong></p>
+                            <p style={{fontSize:20}} ><strong>Horário de Funcionamento</strong></p>
+                            <p>Segunda a Quinta das 16:00 às 20:00</p>
+                            <p>Sexta, Domingo e Feriados das 16:00 às 22:00</p>
+                        </Col>
+                    </Row>
+                    <Button style={{background:'#FF414D', border:0, width:200, borderRadius:8 }} onClick={handleNovoPedido}>Novo Pedido</Button>
                 </Col>
                 <Col/>
             </Row>
--- src/pages/Pedido/index.js
+++ src/pages/Pedido/index.js
@@ -1,8 +1,103 @@
 import React from 'react';
 import Logo from '../../img/Logo.jpeg'
+import Pastel from '../../img/pastel.jpg'
+import Add from '../../img/add.png'
+import {Button, Container, Row, Col, Image} from 'react-bootstrap';
+
 import './style.css'
 
 export default function Pedido (){
+
+    return(
+        <Container>
+            <Row style={{textAlign:'center', paddingBottom:10}}>
+                <Col md='2'></Col>
+                <Col md='8' style={{background:'#FFF', padding:20, borderRadius:8}}>
+                    <Image src={Logo} style={{height:50, marginBottom:20, width:50}} alt='logo' />
+                    <Row>
+                        <Col>
+                            <p className='cabecalho-titulo'><strong>Pastelaria Fina Massa</strong></p>
+                            <p className='cabecalho-subtitulo'>Novo Pedido</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col style={{width:'100%', height:0.5, background:'#F3B442', marginTop:10, marginBottom:10}}/>
+                    </Row>
+                    <Row style={{margin:0}}>
+                        <Col style={{padding:0}}>
+                            <ul className="lista-produtos" style={{padding:0,}} >
+                                <li style={{listStyle:'none'}}>
+                                    <Row style={{borderBottomStyle:'solid', borderBottomColor:'#e3e3e3', borderBottomWidth:0.5, paddingTop:5, paddingBottom:5}}>
+                                        <Col xs='2' style={{padding:5}}>
+                                            <Image src={Pastel} roundedCircle style={{height:50, width:50}} alt='logo' />
+                                        </Col>
+                                        <Col xs='5' style={{ }}>
+                                            <p><strong>Pastel de Carne</strong></p>
+                                            <p style={{fontSize:15}}>Carne, Cebola e Cheiro Verde</p>
+                                        </Col>
+                                        <Col xs='3' style={{padding:15}}>
+                                            <p style={{fontSize:20}}>R$<strong>3,00</strong></p>
+                                        </Col>
+                                        <Col xs='2' style={{paddingTop:15}}>
+                                        <Image src={Add} roundedCircle style={{height:30, width:30}} alt='Adicionar' />
+                                        </Col>
+                                    </Row>
+                                </li>
+                                <li style={{listStyle:'none'}}>
+                                    <Row style={{borderBottomStyle:'solid', borderBottomColor:'#e3e3e3', borderBottomWidth:0.5, paddingTop:5, paddingBottom:5}}>
+                                        <Col xs='2' style={{padding:5}}>
+                                            <Image src={Pastel} roundedCircle style={{height:50, width:50}} alt='logo' />
+                                        </Col>
+                                        <Col xs='5' style={{ }}>
+                                            <p><strong>Pastel de Carne</strong></p>
+                                            <p style={{fontSize:15}}>Carne, Cebola e Cheiro Verde</p>
+                                        </Col>
+                                        <Col xs='3' style={{padding:15}}>
+                                            <p style={{fontSize:20}}>R$<strong>3,00</strong></p>
+                                        </Col>
+                                        <Col xs='2' style={{paddingTop:15}}>
+                                        <Image src={Add} roundedCircle style={{height:30, width:30}} alt='Adicionar' />
+                                        </Col>
+                                    </Row>
+                                </li>
+                                <li style={{listStyle:'none'}}>
+                                    <Row style={{borderBottomStyle:'solid', borderBottomColor:'#e3e3e3', borderBottomWidth:0.5, paddingTop:5, paddingBottom:5}}>
+                                        <Col xs='2' style={{padding:5}}>
+                                            <Image src={Pastel} roundedCircle style={{height:50, width:50}} alt='logo' />
+                                        </Col>
+                                        <Col xs='5' style={{ }}>
+                                            <p><strong>Pastel de Carne</strong></p>
+                                            <p style={{fontSize:15}}>Carne, Cebola e Cheiro Verde</p>
+                                        </Col>
+                                        <Col xs='3' style={{padding:15}}>
+                                            <p style={{fontSize:20}}>R$<strong>3,00</strong></p>
+                                        </Col>
+                                        <Col xs='2' style={{paddingTop:15}}>
+                                        <Image src={Add} roundedCircle style={{height:30, width:30}} alt='Adicionar' />
+                                        </Col>
+                                    </Row>
+                                </li>
+                            </ul>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col/>
+                        <Col md='6'>
+                            <Button>
+                                Ir para a Cesta R$13,00
+                            </Button>
+                        </Col>
+                        <Col/>
+                    </Row>
+                </Col>
+                <Col md='2'></Col>
+            </Row>
+        </Container>
+    )
+
+
+
+
 
     return(
         <div className='container-geral'>
@@ -16,51 +111,6 @@ export default function Pedido (){
             </div>
             <div className='separador'/>
             <ul className="lista-produtos">
-                <li>
-                    <div className='container-item'>
-                        <div className='container-img-produto'>
-                            <img className='img-produto' src={Logo} alt='produto'/>
-                        </div>
-                        <div className='container-descricao'>
-                            <p><strong>Pastel de Carne</strong></p>
-                            <p>Carne, Cebola e Cheiro Verde</p>
-                        </div>
-                        <div className='container-valor'>
-                            <p><strong>R$ 3,00</strong></p>
-                        </div>
-                    </div>
-                    <div className='separador-item'/>
-                </li>
-                <li>
-                    <div className='container-item'>
-                        <div className='container-img-produto'>
-                            <img className='img-produto' src={Logo} alt='produto'/>
-                        </div>
-                        <div className='container-descricao'>
-                            <p><strong>Pastel de Carne</strong></p>
-                            <p>Carne, Cebola e Cheiro Verde</p>
-                        </div>
-                        <div className='container-valor'>
-                            <p><strong>R$ 3,00</strong></p>
-                        </div>
-                    </div>
-                    <div className='separador-item'/>
-                </li>
-                <li>
-                    <div className='container-item'>
-                        <div className='container-img-produto'>
-                            <img className='img-produto' src={Logo} alt='produto'/>
-                        </div>
-                        <div className='container-descricao'>
-                            <p><strong>Pastel de Carne</strong></p>
-                            <p>Carne, Cebola e Cheiro Verde</p>
-                        </div>
-                        <div className='container-valor'>
-                            <p><strong>R$ 3,00</strong></p>
-                        </div>
-                    </div>
-                    <div className='separador-item'/>
-                </li>
                 <li>
                     <div className='container-item'>
                         <div className='container-img-produto'>
