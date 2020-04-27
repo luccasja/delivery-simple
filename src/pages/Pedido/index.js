52189e40d59196c6e340a590af1fe824684767ff
Lucas Almeida <luccasja@live.com>
2020-04-26 22:21:08 -0300

    Ajustes na janela pedido

--- src/pages/Pedido/index.js
+++ src/pages/Pedido/index.js
@@ -13,77 +13,47 @@ export default function Pedido (){
             <Row style={{textAlign:'center', paddingBottom:10}}>
                 <Col md='2'></Col>
                 <Col md='8' style={{background:'#FFF', padding:20, borderRadius:8}}>
-                    <Image src={Logo} style={{height:50, marginBottom:20, width:50}} alt='logo' />
                     <Row>
-                        <Col>
+                        <Col xs='2' >
+                            <Image src={Logo} style={{height:50, width:50}} alt='logo' />
+                        </Col>
+                        <Col xs='8'>
                             <p className='cabecalho-titulo'><strong>Pastelaria Fina Massa</strong></p>
                             <p className='cabecalho-subtitulo'>Novo Pedido</p>
                         </Col>
+                        <Col xs='2' ></Col>
                     </Row>
                     <Row>
-                        <Col style={{width:'100%', height:0.5, background:'#F3B442', marginTop:10, marginBottom:10}}/>
+                        <Col style={{width:'100%', height:0.5, background:'#F3B442', marginTop:10, marginBottom:0}}/>
                     </Row>
                     <Row style={{margin:0}}>
-                        <Col style={{padding:0}}>
-                            <ul className="lista-produtos" style={{padding:0,}} >
+                        <Col style={{padding:0,  overflow:'auto'}}>
+                            <ul className="lista-produtos" style={{padding:0, width:'96%', height:400}}>
                                 <li style={{listStyle:'none'}}>
                                     <Row style={{borderBottomStyle:'solid', borderBottomColor:'#e3e3e3', borderBottomWidth:0.5, paddingTop:5, paddingBottom:5}}>
-                                        <Col xs='2' style={{padding:5}}>
+                                        <Col xs='3' style={{padding:5}}>
                                             <Image src={Pastel} roundedCircle style={{height:50, width:50}} alt='logo' />
                                         </Col>
                                         <Col xs='5' style={{ }}>
                                             <p><strong>Pastel de Carne</strong></p>
                                             <p style={{fontSize:15}}>Carne, Cebola e Cheiro Verde</p>
                                         </Col>
-                                        <Col xs='3' style={{padding:15}}>
-                                            <p style={{fontSize:20}}>R$<strong>3,00</strong></p>
+                                        <Col xs='2' style={{padding:15}}>
+                                            <p style={{fontSize:20}}><strong>3,00</strong></p>
                                         </Col>
-                                        <Col xs='2' style={{paddingTop:15}}>
-                                        <Image src={Add} roundedCircle style={{height:30, width:30}} alt='Adicionar' />
-                                        </Col>
-                                    </Row>
-                                </li>
-                                <li style={{listStyle:'none'}}>
-                                    <Row style={{borderBottomStyle:'solid', borderBottomColor:'#e3e3e3', borderBottomWidth:0.5, paddingTop:5, paddingBottom:5}}>
-                                        <Col xs='2' style={{padding:5}}>
-                                            <Image src={Pastel} roundedCircle style={{height:50, width:50}} alt='logo' />
-                                        </Col>
-                                        <Col xs='5' style={{ }}>
-                                            <p><strong>Pastel de Carne</strong></p>
-                                            <p style={{fontSize:15}}>Carne, Cebola e Cheiro Verde</p>
-                                        </Col>
-                                        <Col xs='3' style={{padding:15}}>
-                                            <p style={{fontSize:20}}>R$<strong>3,00</strong></p>
-                                        </Col>
-                                        <Col xs='2' style={{paddingTop:15}}>
-                                        <Image src={Add} roundedCircle style={{height:30, width:30}} alt='Adicionar' />
+                                        <Col xs='2' style={{paddingTop:18}}>
+                                        <Image src={Add} roundedCircle style={{height:25, width:25}} alt='Adicionar' />
                                         </Col>
                                     </Row>
                                 </li>
-                                <li style={{listStyle:'none'}}>
-                                    <Row style={{borderBottomStyle:'solid', borderBottomColor:'#e3e3e3', borderBottomWidth:0.5, paddingTop:5, paddingBottom:5}}>
-                                        <Col xs='2' style={{padding:5}}>
-                                            <Image src={Pastel} roundedCircle style={{height:50, width:50}} alt='logo' />
-                                        </Col>
-                                        <Col xs='5' style={{ }}>
-                                            <p><strong>Pastel de Carne</strong></p>
-                                            <p style={{fontSize:15}}>Carne, Cebola e Cheiro Verde</p>
-                                        </Col>
-                                        <Col xs='3' style={{padding:15}}>
-                                            <p style={{fontSize:20}}>R$<strong>3,00</strong></p>
-                                        </Col>
-                                        <Col xs='2' style={{paddingTop:15}}>
-                                        <Image src={Add} roundedCircle style={{height:30, width:30}} alt='Adicionar' />
-                                        </Col>
-                                    </Row>
-                                </li>
+                                
                             </ul>
                         </Col>
                     </Row>
                     <Row>
                         <Col/>
                         <Col md='6'>
-                            <Button>
+                            <Button style={{marginTop:10, height:45}}>
                                 Ir para a Cesta R$13,00
                             </Button>
                         </Col>
