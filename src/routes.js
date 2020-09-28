1e3e7a68bd087e043217f3eca2fbe811b2ed85f1
Lucas Almeida <luccasja@live.com>
2020-09-27 23:12:47 -0300

    Inclusão do Provider do serviço de notificação

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {ToastProvider} from 'react-toast-notifications'
 import HttpsRedirect from 'react-https-redirect';
 import Home from './pages/Home'
 import Login from './pages/Login'
@@ -9,18 +10,21 @@ import Finalizar from './pages/Finalizar'
 import Dashboard from './pages/Dashboard'
 
 export default function Routes() {
+    
     return(
         <HttpsRedirect>
-            <BrowserRouter>
-                <Switch>
-                    <Route path='/' exact component={Home} />
-                    <Route path='/login' component={Login} />
-                    <Route path='/pedido' component={Pedido} />
-                    <Route path='/pb/:id' component={PedidoPublic} />
-                    <Route path='/finalizar' component={Finalizar} />
-                    <Route path='/dashboard' component={Dashboard} />
-                </Switch>
-            </BrowserRouter>
+            <ToastProvider>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path='/' exact component={Home} />
+                        <Route path='/login' component={Login} />
+                        <Route path='/pedido' component={Pedido} />
+                        <Route path='/pb/:id' component={PedidoPublic} />
+                        <Route path='/finalizar' component={Finalizar} />
+                        <Route path='/dashboard' component={Dashboard} />
+                    </Switch>
+                </BrowserRouter>
+            </ToastProvider>
         </HttpsRedirect>
     )
 }
